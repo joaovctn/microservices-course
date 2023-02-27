88d5ae81170135a1afeaf824cbd5f88ae601c4e7
João Victor <jvpinheirodossantos@gmail.com>
2023-02-27 16:03:17 -0300

    Feign Client

--- hr-payroll/java/com/example/hrpayroll/entities/Payment.java
+++ hr-payroll/java/com/example/hrpayroll/entities/Payment.java
@@ -1,6 +1,10 @@
 package com.example.hrpayroll.entities;
 
-public class Payment {
+import java.io.Serializable;
+
+public class Payment implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	private String name;
 	private Double dailyIncome;
 	private Integer days;
--- hr-payroll/java/com/example/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/java/com/example/hrpayroll/resources/PaymentResource.java
@@ -13,13 +13,13 @@ import com.example.hrpayroll.services.PaymentService;
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
-
+	
 	@Autowired
 	private PaymentService service;
 	
-	@GetMapping(value = "/{workerId}/days/{days}")
-	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+	@GetMapping(value="/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
-	}	
+	}
 }
